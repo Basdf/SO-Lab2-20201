8a3f9ea17ce6d55fe75426267d4bb8f2a2f5f109
Eduardo Velasquez <eduardo.velasquezv@udea.edu.co>
2020-10-15 18:55:05 -0500

    Cambio menor

--- ejercicios_refuerzo/time.c
+++ ejercicios_refuerzo/time.c
@@ -6,22 +6,23 @@
 
 int value = 5;
 int main(int argc, char *argv[]) {
+
     int pid;
     struct timeval current_time;
     gettimeofday(&current_time, NULL);
     int timeI=current_time.tv_usec;
     pid = fork();
     char *argv2[argc-1];
+
     for (int i = 1; i < argc; i++){
         argv2[i-1]=argv[i];
     }
     
     if (pid == 0) { /* child process */
         execvp(argv2[0],argv2);
-        
-        printf("%s\n",argv[0]);
-        
-        
+        for (int i = 1; i < argc; i++){
+            printf("%s ",argv2[i]);
+        }
     }
     else if (pid > 0) { /* parent process */
         wait(NULL);
